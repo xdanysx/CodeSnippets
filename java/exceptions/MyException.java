d16580112d68f6423e66a84c0874fcaae3ca10dc
Daniel <137919461+xdanysx@users.noreply.github.com>
2023-07-21 11:27:28 +0200

    Add files via upload

---
+++ java/exceptions/MyException.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+@SuppressWarnings("serial")
+public class MyException extends Exception{
+	
+	private String errorText;
+	private int errorCode;
+	
+	public MyException(String pErrorText, int pErrorCode){
+		super(pErrorText);
+		this.errorText = pErrorText;
+		this.errorCode = pErrorCode;
+	}
+	
+	public String getMessage() {
+		return this.errorText;	
+	}
+	
+	public int getErrorCode() {
+		return this.errorCode;
+	}
+
+}
