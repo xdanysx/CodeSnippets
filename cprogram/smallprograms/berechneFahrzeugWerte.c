a4988f9c1b31f96ffd0738ee730a87ac1c3a59ab
Daniel <137919461+xdanysx@users.noreply.github.com>
2025-01-16 14:20:30 +0100

    Add files via upload

---
+++ cprogram/smallprograms/berechneFahrzeugWerte.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+// Berechnet 10 Werte aus, die Sekuendlich ausgegeben werden
+float berechneWert(float a){
+    for(int t = 0; t < 10; t++){
+        float v = a * t;
+        float s = (1/2.0f) * a * (float)t * (float)t;
+        printf("Nach %d Sekunden hat das Auto %f Meter hinterlegt und ist %f km/h schnell \n", t, s, v );
+    }
+}
+
+int main() {
+    float gasPedal = 0.5f; // Beschreibt wie stark das GasPedal gedrückt worden ist
+    float beschleunigung = 29.0f; // Gibt die beschleunigung in m/s*s an
+    
+    berechneWert(beschleunigung * gasPedal);
+    return 0;
+}
+
